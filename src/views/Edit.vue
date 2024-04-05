c8069ccc48822d1da0372db4a14382b80e3295c1
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2024-04-05 22:16:43 +0200

    fin digga

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="padtop">
+  <form class="padtop" @submit.prevent="back">
     <h3 class="lig">{{ student.name }} {{ student.surname }} bearbeiten:</h3>
     <hr>
     <div class="uk-margin">
@@ -20,9 +20,9 @@
       Preis: {{ (green * Number(route.params.green_price)) + (gold * Number(route.params.gold_price)) }} €
     </div>
     <hr>
-    <button type="button" class="uk-button uk-button-danger uk-width-1-1 uk-margin-small-bottom" @click="back">Zurück</button>
+    <button type="button" class="uk-button uk-button-danger uk-width-1-1 uk-margin-small-bottom">Zurück</button>
     <br>
-    <button type="submit" class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom" @click="save" @submit.prevent>Speichern</button>
+    <button type="submit" class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom" @click="save">Speichern</button>
   </form>
 </template>
 
@@ -47,12 +47,12 @@ const gold: Ref<number> = ref(0);
 
 const save = async () => {
   student.value.points += (green.value * Number(route.params.green)) + (gold.value * Number(route.params.gold));
-  await axios.put(`api/students/${route.params.id}`, student.value);
+  await axios.put(`http://${window.location.hostname}:5001/students/${route.params.id}`, student.value);
   back();
 };
 
 const fetchStudent = async () => {
-  const response = await axios.get(`api/students/${route.params.id}`);
+  const response = await axios.get(`http://${window.location.hostname}:5001/students/${route.params.id}`);
   student.value = response.data;
 };
 
